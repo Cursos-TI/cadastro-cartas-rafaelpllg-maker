e7ddddfacf620473ce330d8ab532c90e2c1a0eb9
Rafael Pellegrino <rafael.pllg@gmail.com>
2026-04-17 17:48:32 +0000

    Alteração do nível Mestre
    Adicionei o cálculo do "Supor Poder" da cidade
    
    Adicionei a funcionalidade de comparação entre as
    cartas para imprimir os resultados na tela,
    
    Modifiquei o tipo da variável População de int
    simples para unsigned long int
    
    Modifiquei o tipo das variáveis 'densidade_pop' e
    'pib_capta' para double, para evitar imprecisões
    ao serem calculadas com números muito grandes vindos
    da variável 'populacao', que agora é unsigned long int.
    
    Tratei a inconsistência nos empates, que
    davam o "ponto" para a carta 2.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+// Nível Mestre - Comparação das cartas
+// Objetivo: No nível Mestre, você implementará comparações entre as cartas, utilizando operadores relacionais e manipulando grandes números com precisão.
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
@@ -10,18 +10,21 @@ int main() {
   char cod_estado1= 'a', cod_estado2= 'a';            //Variáveis com valores de "A" a "H" para os Estados
   char cod_carta1[4], cod_carta2[4];                  //Variáveis com valores de "01" a "04" para as cidades
   char nome_cidade1[50], nome_cidade2[50];            //Nome da cidade
-  int populacao1= 0, populacao2 = 0;                  //Variáveis com a população
+  unsigned long int populacao1= 0, populacao2 = 0;    //Variáveis com a população
   int pts_turisticos1 = 0, pts_turisticos2 = 0;       //Variáveis com os pontos turísticos
   float pib1 = 0, pib2 = 0;                       //Variáveis com o PIB
   float area1 = 0, area2 = 0;                     //Variáveis com área
   float densidade_pop1 = 0, densidade_pop2 = 0;   //Variável calculada: populacao/area
   float pib_capta1 = 0, pib_capta2 = 0;           //variável calculada: pib/populacao
   float bilhao = 1000000000;                      //Transforma a notação do PIB de bilhões de reais para unidades de real (ex.: 600 bi para 600.000.000.000)
+  double super1, super2 = 0;                                //Variável calculada: o 'super poder' da carta com a soma do inverso da densidade com todos os outros atributos da carta
+  int comparaPop, comparaArea, comparaPib, comparaTur, comparaDens, comparaPcap, comparaSup = 0;
+
 
   // Área para entrada de dados
   //Cabeçalho do programa
   printf("===================================================\n");
-  printf("|          Bem-vindo ao Super Trunfo 0.2          |\n");
+  printf("|          Bem-vindo ao Super Trunfo 0.3          |\n");
   printf("===================================================\n\n");
     
   printf("Escolha duas cartas da sua mão e insira os valores\nde cada item conforme as instrucões a seguir:\n\n");
@@ -38,7 +41,7 @@ int main() {
   printf("Insira o nome da cidade:\n");
   scanf(" %[^\n]", nome_cidade1);
   printf("Insira a quantidade da População:\n");
-  scanf("%d", &populacao1);
+  scanf("%lu", &populacao1);
   printf("Insira o valor da Área (em Km²):\n");
   scanf("%f", &area1);
   printf("Insira o valor do PIB:\n");
@@ -62,7 +65,7 @@ int main() {
   printf("Insira o nome da cidade:\n");
   scanf(" %[^\n]", nome_cidade2);
   printf("Insira a quantidade da População:\n");
-  scanf("%d", &populacao2);
+  scanf("%lu", &populacao2);
   printf("Insira o valor da área (em Km²):\n");
   scanf("%f", &area2);
   printf("Insira o valor do PIB:\n");
@@ -79,20 +82,64 @@ int main() {
   printf("======================================================\n\n");
 
   //Processando os dados da Carta 1: cálculo do PIB per capta e da densidade populacional
-  densidade_pop1 = (float) populacao1 / area1;
-  pib_capta1  = (float) (pib1 * bilhao) / populacao1;
+  densidade_pop1 = (double) populacao1 / area1;
+  pib_capta1  = (double) (pib1 * bilhao) / populacao1;
+  super1 = (double) (1/densidade_pop1) + populacao1 + area1 + pib1 + pts_turisticos1 + pib_capta1;
 
   //Processando os dados da Carta 1: cálculo do PIB per capta e da densidade populacional
-  densidade_pop2 = (float) populacao2 / area2;
-  pib_capta2 = (float) (pib2 * bilhao) / populacao2;
+  densidade_pop2 = (double) populacao2 / area2;
+  pib_capta2 = (double) (pib2 * bilhao) / populacao2;
+  super2 = (double) (1/densidade_pop2) + populacao2 + area2 + pib2 + pts_turisticos2 + pib_capta2;
+
+  /*
+    
+  Processando os dados: sistema de comparação entre os atributos das cartas:
+
+  OBS.: Para as comparações foi necessário resolver uma inconsistência nos casos de empate:
+  A comparação "a > b" ou "a < b" retorna 0 quando é o caso que "b == a", o que daria
+  vantagem à carta 2, de acordo com a saída proposta no exercício. Para respeitar os
+  limites do desafio e usar somente o que foi passado em aula, a solução pensada foi
+  usar a propriedade de 1 e 0 como números inteiros e empregar operadores aritméticos
+  como auxílio (pois ainda não foram passados operadores lógicos).
+  
+  Dessa forma, a minha ideia foi estruturar operações nas variáveis 'comparaX'
+  para que elas devolvessem 3 valores, e não 2:
+  
+  um valor x para pontuação da carta 1
+  um valor y para a carta 2
+  um valor z para empate
+  
+  A princípio surgiu a solução do tipo 'compara = (a > b) - (a < b)'
+  (que devolveria -1 para vitória da carta 2, 0 para empate e 1 para
+  vitória da carta 1), mas para respeitar a saída que foi pedida no
+  desafio, usei outra solução: 'compara = (a > b) + 2*(a == b)', onde
+  as pontuações das cartas 1 e 2 ainda seguem 1 e 0, respectivamente,
+  e o valor do empate é 2. O Valor 2 se torna assim uma 'flag' para
+  'empate', mas as saídas propostas no desafio: 1 para vitória da carta 1
+  e 0 para vitória da carta 2 permanecem respeitadas.
+  */
+  comparaPop = (populacao1 > populacao2) + 2*(populacao1 == populacao2);
+  comparaArea = (area1 > area2) + 2*(area1 == area2);
+  comparaPib = (pib1 > pib2) + 2*(pib1 == pib2);
+  comparaTur = (pts_turisticos1 > pts_turisticos2) + 2*(pts_turisticos1 == pts_turisticos2);
+  comparaDens = (densidade_pop1 < densidade_pop2) + 2*(densidade_pop1 == densidade_pop2);
+  comparaPcap = (pib_capta1 > pib_capta2) + 2*(pib_capta1 == pib_capta2);
+  comparaSup = (super1 > super2) + 2*(super1 == super2);
 
   // Área para exibição dos dados da cidade
-  printf("Carta:1\nEstado: %c\nCódigo da carta: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de pontos turísticos: %d\n", cod_estado1, cod_carta1, nome_cidade1, populacao1, area1, pib1, pts_turisticos1);
-  printf("Densidade Populacional: %.2f hab/km²\nPIB per Capta: %.2f reais", densidade_pop1, pib_capta1);
+  printf("Carta:1\nEstado: %c\nCódigo da carta: %s\nNome da cidade: %s\nPopulação: %lu\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de pontos turísticos: %d\n", cod_estado1, cod_carta1, nome_cidade1, populacao1, area1, pib1, pts_turisticos1);
+  printf("Densidade Populacional: %.2f hab/km²\nPIB per Capta: %.2f reais\nSuper Poder: %.2f", densidade_pop1, pib_capta1, super1);
   printf("\n\n");
-  printf("Carta:2\nEstado: %c\nCódigo da carta: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de pontos turísticos: %d\n", cod_estado2, cod_carta2, nome_cidade2, populacao2, area2, pib2, pts_turisticos2);
-  printf("Densidade Populacional: %.2f hab/km²\nPIB per Capta: %.2f reais", densidade_pop2, pib_capta2);
+  printf("Carta:2\nEstado: %c\nCódigo da carta: %s\nNome da cidade: %s\nPopulação: %lu\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de pontos turísticos: %d\n", cod_estado2, cod_carta2, nome_cidade2, populacao2, area2, pib2, pts_turisticos2);
+  printf("Densidade Populacional: %.2f hab/km²\nPIB per Capta: %.2f reais\nSuper Poder: %.2f", densidade_pop2, pib_capta2, super2);
   printf("\n\n");
+
+  printf("______________________________________________________\n\n");
+
+  //Saída dos dados da comparação das cartas:
+  printf("COMPARAÇÃO DAS CARTAS!!\n\n");
+  printf("LEGENDA:\nResultado = 1: Carta 1 venceu.\nResultado = 0: Carta 2 venceu.\nResultado = 2: Empate.\n\n");
+  printf("População: %d\nÁrea: %d\nPIB: %d\nPontos Turísticos: %d\nDensidade Populacional: %d\nPIB per Capta: %d\nSuper Poder: %d\n\n", comparaPop, comparaArea, comparaPib, comparaTur, comparaDens, comparaPcap, comparaSup);
 
   printf("======================================================\n\n");
   printf("======================================================\n\n");
